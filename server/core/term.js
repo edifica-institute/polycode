ba5fd36ae194a53f12f3046a203fa1a7a1e965ed
Edifica <edifica.institute@gmail.com>
2025-08-21 00:05:11 +0530

    Delete server/core/term.js

--- server/core/term.js
+++
@@ -1,6 +0,0 @@
-exports.register = (app) => {
-  const { WebSocketServer } = require('ws');
-  const wss = new WebSocketServer({ noServer: true });
-  // Stub WS handler
-  console.log("WebSocket /term initialized");
-};
