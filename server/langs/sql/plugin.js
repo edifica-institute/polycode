8eb4a21248869929a2f2c12fce42f0f90d90be0f
Edifica <edifica.institute@gmail.com>
2025-08-21 01:56:03 +0530

    Update plugin.js

--- server/langs/sql/plugin.js
+++ server/langs/sql/plugin.js
@@ -1,6 +1,7 @@
-// server/langs/sql/plugin.js
-export async function register(app, core){
+// server/langs/sql/plugin.js (CJS)
+function register(app, _core){
   app.post('/api/sql/prepare', (req, res) => {
     res.json({ ok: true, note: 'SQL runs client-side via sql.js (WASM).' });
   });
 }
+module.exports = { register };
