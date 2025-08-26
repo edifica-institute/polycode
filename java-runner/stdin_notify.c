0234fdf52471d2c11e067f774529e401c890a971
Edifica <edifica.institute@gmail.com>
2025-08-26 20:06:46 +0530

    Update stdin_notify.c

--- java-runner/stdin_notify.c
+++ java-runner/stdin_notify.c
@@ -3,13 +3,11 @@
 #include <dlfcn.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/uio.h>
 #include <stddef.h>
 #include <string.h>
 #include <stdatomic.h>
 
 typedef ssize_t (*read_fn)(int, void*, size_t);
-
 static read_fn real_read = NULL;
 static _Atomic int notified = 0;
 
@@ -19,19 +17,13 @@ static void init(void) {
 }
 
 ssize_t read(int fd, void *buf, size_t count) {
-  if (!real_read) {
-    real_read = (read_fn)dlsym(RTLD_NEXT, "read");
-  }
-  // Only announce when reading from STDIN (fd 0)
+  if (!real_read) real_read = (read_fn)dlsym(RTLD_NEXT, "read");
   if (fd == 0 && atomic_load(&notified) == 0) {
     static const char *msg = "[[CTRL]]:stdin_req\n";
-    (void)write(2, msg, strlen(msg));   // to stderr
+    (void)write(2, msg, strlen(msg));   // announce on stderr
     atomic_store(&notified, 1);
   }
   ssize_t r = real_read(fd, buf, count);
-  // Once some bytes are read, allow future notifications
-  if (fd == 0 && r > 0) {
-    atomic_store(&notified, 0);
-  }
+  if (fd == 0 && r > 0) atomic_store(&notified, 0); // allow repeated prompts
   return r;
 }
