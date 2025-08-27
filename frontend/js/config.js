83a187535fd558d2d034d6f288be056ccd5b473f
Edifica <edifica.institute@gmail.com>
2025-08-27 17:53:00 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,5 @@
 export const API_BASE = 'https://polycode.onrender.com';
 export const WS_BASE  = 'wss://polycode.onrender.com';
+
+export const CC_API_BASE = 'https://polycode-cc.onrender.com';
+export const CC_WS_URL   = 'wss://polycode-cc.onrender.com/cc';
